e4afef25fbbab2da71d5d2dff0af751602b2685e
javaccy <javaccy@gmail.com>
2019-02-27 09:55:26 +0800

    Update kube-images.sh

--- kube-images.sh
+++ kube-images.sh
@@ -29,10 +29,10 @@ docker pull mirrorgooglecontainers/etcd-amd64:$etcdVersion &&
 docker pull coredns/coredns:$corednsVersion &&
 
 
-docker tag docker.io/mirrorgooglecontainers/kube-proxy-amd64:$version k8s.gcr.io/kube-proxy-amd64:$version &&
-docker tag docker.io/mirrorgooglecontainers/kube-scheduler-amd64:$version k8s.gcr.io/kube-scheduler-amd64:$version &&
-docker tag docker.io/mirrorgooglecontainers/kube-apiserver-amd64:$version k8s.gcr.io/kube-apiserver-amd64:$version &&
-docker tag docker.io/mirrorgooglecontainers/kube-controller-manager-amd64:$version k8s.gcr.io/kube-controller-manager-amd64:$version &&
-docker tag docker.io/mirrorgooglecontainers/etcd-amd64:$etcdVersion  k8s.gcr.io/etcd-amd64:$etcdVersion &&
+docker tag docker.io/mirrorgooglecontainers/kube-proxy-amd64:$version k8s.gcr.io/kube-proxy:$version &&
+docker tag docker.io/mirrorgooglecontainers/kube-scheduler-amd64:$version k8s.gcr.io/kube-scheduler:$version &&
+docker tag docker.io/mirrorgooglecontainers/kube-apiserver-amd64:$version k8s.gcr.io/kube-apiserver:$version &&
+docker tag docker.io/mirrorgooglecontainers/kube-controller-manager-amd64:$version k8s.gcr.io/kube-controller-manager:$version &&
+docker tag docker.io/mirrorgooglecontainers/etcd-amd64:$etcdVersion  k8s.gcr.io/etcd:$etcdVersion &&
 docker tag docker.io/mirrorgooglecontainers/pause:$pauseVersion  k8s.gcr.io/pause:$pauseVersion &&
 docker tag docker.io/coredns/coredns:$corednsVersion  k8s.gcr.io/coredns:$corednsVersion
