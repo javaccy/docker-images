06388269cb67fe148a868e1c811bf6c3291cbe0f
javaccy <javaccy@gmail.com>
2019-02-27 09:58:04 +0800

    Update kube-images.sh

--- kube-images.sh
+++ kube-images.sh
@@ -4,21 +4,22 @@ pauseVersion=3.1
 etcdVersion=3.2.24
 corednsVersion=1.2.6
 
-docker rmi k8s.gcr.io/kube-apiserver-amd64:$version
-docker rmi k8s.gcr.io/kube-controller-manager-amd64:$version
-docker rmi k8s.gcr.io/kube-scheduler-amd64:$version
-docker rmi k8s.gcr.io/kube-proxy-amd64:$version
+
+docker rmi k8s.gcr.io/kube-apiserver:$version
+docker rmi k8s.gcr.io/kube-controller-manager:$version
+docker rmi k8s.gcr.io/kube-scheduler:$version
+docker rmi k8s.gcr.io/kube-proxy:$version
 docker rmi k8s.gcr.io/pause:$pauseVersion
-docker rmi k8s.gcr.io/etcd-amd64:$etcdVersion
+docker rmi k8s.gcr.io/etcd:$etcdVersion
 docker rmi k8s.gcr.io/coredns:$corednsVersion
 
-docker mirrorsgooglecontainers/kube-apiserver-amd64:$version
-docker mirrorsgooglecontainers/kube-controller-manager-amd64:$version
-docker mirrorsgooglecontainers/kube-scheduler-amd64:$version
-docker mirrorsgooglecontainers/kube-proxy-amd64:$version
-docker mirrorsgooglecontainers/pause:$pauseVersion
-docker mirrorsgooglecontainers/etcd-amd64:$etcdVersion
-docker coredns/coredns:$corednsVersion
+docker rmi mirrorsgooglecontainers/kube-apiserver-amd64:$version
+docker rmi mirrorsgooglecontainers/kube-controller-manager-amd64:$version
+docker rmi mirrorsgooglecontainers/kube-scheduler-amd64:$version
+docker rmi mirrorsgooglecontainers/kube-proxy-amd64:$version
+docker rmi mirrorsgooglecontainers/pause:$pauseVersion
+docker rmi mirrorsgooglecontainers/etcd-amd64:$etcdVersion
+docker rmi coredns/coredns:$corednsVersion
 
 docker pull mirrorgooglecontainers/kube-apiserver-amd64:$version &&
 docker pull mirrorgooglecontainers/kube-controller-manager-amd64:$version &&
